c0f8e995d0c6d8cce7526a51452c3e11596056eb
Daniela Sainz <dasainz18@gmail.com>
2018-04-11 16:38:53 -0400

    Initial commit

--- src/com/Sainz/Main.java
+++ src/com/Sainz/Main.java
@@ -5,17 +5,15 @@ package com.Sainz;
         Show the following:
         THESE ARE THE INSTRUCTIONS
 
-        Your date: [Date entered by the user]
-        The day of the week on which this day falls: [Day of the week]
-        The month of the year in which this day falls: [Month - full name]
-        The date is a [future or past] date.
+       [1] Your date: [Date entered by the user]
+       [2] The day of the week on which this day falls: [Day of the week]
+       [3] The month of the year in which this day falls: [Month - full name]
+       [4] The date is a [future or past] date.
 
         */
 import java.time.LocalDate;
 import java.util.Scanner;
 import java.time.format.DateTimeFormatter;
-
-
 
 public class Main {
 
@@ -27,29 +25,14 @@ public class Main {
         DateTimeFormatter dTF = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         DateTimeFormatter longFormat = DateTimeFormatter.ofPattern("dd MMMM yyyy");
 
-        System.out.println("Now enter a date in the format dd/MM/YYYY");
+        System.out.println("Enter a date in the format dd/MM/YYYY");
         String aDate = keyboard.nextLine();
         userDate = LocalDate.parse(aDate, dTF);
         System.out.println(userDate.format(longFormat));
 
-        /*
-
-
-        java.util.Date  = df.parse(userDate.format(longFormat));
-        df.applyPattern("EEE");
-        System.out.println("Day of Week: " + df.format(userDate.format(longFormat)));
-
-        /*
-
-        SimpleDateFormat df = new SimpleDateFormat( "dd/MM/yy" );
-//parse in the date
-java.util.Date date = df.parse( "06/08/2002");
-//change the pattern to output the day of week
-df.applyPattern( "EEE" );
-//print the formatted date out
-System.out.println( "Day of Week = " + df.format( date ) );
-         */
-
+        //Information from Oracle
+        System.out.println("This day falls in this month of the year: " + userDate.getMonth().name());
+        System.out.println("This day falls on this day of the week: " + userDate.getDayOfWeek().name());
 
     }
 }
